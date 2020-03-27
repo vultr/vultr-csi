02f6ee531e13025a9d285e93e63c82e603317885
Mariah <mclaugm3@tcnj.edu>
2020-03-27 16:30:55 -0400

    initial driver unit test

---
+++ driver/driver_test.go
@@ -0,0 +1,93 @@
+package driver
+
+import (
+	"context"
+	"math/rand"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/vultr/govultr"
+	"golang.org/x/sync/errgroup"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func TestDriverSuite(t *testing.T) {
+	socket := "/tmp/csi.sock"
+	endpoint := "unix://" + socket
+	if err := os.Remove(socket); err != nil && !os.IsNotExist(err) {
+		t.Fatalf("failed to remove unix domain socket %s, error: %s", socket, err)
+	}
+
+	nodeID := "123456"
+	region := "1"
+	token := "dummy"
+	version := "dev"
+	client := govultr.NewClient(nil, token)
+
+	log := logrus.New().WithFields(logrus.Fields{
+		"region":  "1",
+		"host_id": "12345",
+		"version": "dev",
+	})
+
+	d := &VultrDriver{
+		name:     DefaultDriverName,
+		version:  version,
+		endpoint: endpoint,
+
+		client: client,
+		nodeID: nodeID,
+		region: region,
+
+		waitTimeout: defaultTimeout,
+
+		log:     log,
+		mounter: NewFakeMounter(log),
+	}
+
+	go d.Run()
+
+	_, cancel := context.WithCancel(context.Background())
+
+	var eg errgroup.Group
+
+	cancel()
+	if err := eg.Wait(); err != nil {
+		t.Errorf("driver run failed: %s", err)
+	}
+}
+
+type fakeMounter struct {
+	log     *logrus.Entry
+	mounted map[string]string
+}
+
+func NewFakeMounter(log *logrus.Entry) *fakeMounter {
+	return &fakeMounter{log: log}
+}
+
+func (f *fakeMounter) Format(source, fs string) error {
+	return nil
+}
+
+func (f *fakeMounter) IsFormatted(source string) (bool, error) {
+	return true, nil
+}
+
+func (f *fakeMounter) Mount(source, target, fs string, opts ...string) error {
+	return nil
+}
+
+func (f *fakeMounter) IsMounted(target string) (bool, error) {
+	return true, nil
+}
+
+func (f *fakeMounter) UnMount(target string) error {
+	delete(f.mounted, target)
+	return nil
+}
--- go.mod
+++ go.mod
@@ -9,5 +9,6 @@ require (
 	github.com/stretchr/testify v1.3.0 // indirect
 	github.com/vultr/govultr v0.3.2
 	golang.org/x/net v0.0.0-20190613194153-d28f0bde5980 // indirect
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58
 	google.golang.org/grpc v1.27.1
 )
