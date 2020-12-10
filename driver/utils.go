f9ea6d24aa9fede95da1d85b557e4592698999f5
David Dymko <dymkod@gmail.com>
2020-12-10 12:10:34 -0500

    driver updates

--- driver/utils.go
+++ driver/utils.go
@@ -1,1 +1,18 @@
 package driver
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/vultr/govultr/v2"
+)
+
+func getUUID(ctx context.Context, client *govultr.Client, v1ID string) (string, error) {
+	id := fmt.Sprintf("v1-%s", v1ID)
+	instance, err := client.Instance.Get(ctx, id)
+	if err != nil {
+		return "", err
+	}
+
+	return instance.ID, nil
+}
